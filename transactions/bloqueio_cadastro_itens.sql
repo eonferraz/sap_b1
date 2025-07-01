66fb25a71b56c8e994f46e2f7698d228c7f7a754
Paulo Eduardo Ferraz <56051602+eonferraz@users.noreply.github.com>
2025-07-01 16:34:42 -0300

    Create bloqueio_cadastro_itens.sql

---
+++ transactions/bloqueio_cadastro_itens.sql
@@ -0,0 +1,16 @@
+-- Paulo Ferraz - 01/07/2025 - Bloqueia o usuário qualidade realizar modificação de cadastro
+IF (@object_type = '4' AND (@transaction_type = 'U' OR @transaction_type = 'A'))
+
+BEGIN
+	IF (
+		SELECT COUNT(*)
+		FROM OITM T0
+		INNER JOIN OUSR T1 ON T0.UserSign2 = T1.USERID
+		WHERE T0.ItemCode = @list_of_cols_val_tab_del
+		  AND T1.USERID = 47
+	) > 0
+	BEGIN
+		SET @error = 133
+		SET @error_message = 'Usuário da Qualidade não tem permissão para alterar o cadastro de produtos.';
+	END
+END
